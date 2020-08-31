2b879f3e54dd6cc6b5f0809e1a9819319d96b758
xmilexx <49333165+xmilexx@users.noreply.github.com>
2020-08-31 15:57:52 +0900

    Update tpccstart.sh

--- LEARNED/TPCC-MYSQL/tpccstart.sh
+++ LEARNED/TPCC-MYSQL/tpccstart.sh
@@ -4,7 +4,7 @@
 BASEDIR="/home/song/mysql-5.7.24"
 RESULT_DIR="/home/song/results"
 password="yourpassword"
-./mysql-5.7.24/tpcc-mysql/tpcc_start -h127.0.0.1 -S/tmp/mysql.sock -dtpcc1000 -uroot -p2702 -w1000 -c32 -r10 -l10800 &>$RESULT_DIR/tpcc.txt & echo $password | sudo -S iostat -x 10 10800 &>$RESULT_DIR/iostat.txt
+./mysql-5.7.24/tpcc-mysql/tpcc_start -h127.0.0.1 -S/tmp/mysql.sock -dtpcc1000 -uroot -p2702 -w1000 -c32 -r10 -l10800 &>$RESULT_DIR/tpcc.txt & echo $password | sudo -S iostat -x 10 1080 &>$RESULT_DIR/iostat.txt
 
 sleep 10
 cat $RESULT_DIR/iostat.txt | grep "sdc" | awk '{print $4 $5}' &>$RESULT_DIR/readwrite.txt
